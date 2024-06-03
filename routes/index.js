6847a0738e51a10aa3572c5322bcbb5b06239644
rashid4023 <mamunur@mds.com.bd>
2024-06-03 12:16:35 +0600

    fixed arsing issues

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ const {isUser} = require('../middlewares/auth')
 router.post("/sign-up", userControllers.signUp);
 router.post("/sign-in", userControllers.signIn);
 router.get('/me', isUser, userControllers.userDetail)
-router.post("/logout", isUser, userControllers.logout);
+router.post("/logout", userControllers.logout);
 router.post('/add-history', isUser, userControllers.addSearchHistory)
 
 router.use("*", (req, res, next)=>{
